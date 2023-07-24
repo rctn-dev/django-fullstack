760d30c548f440fa896a2d428c55108895727da3
rctn <coderctn@gmail.com>
2023-07-24 22:09:32 +0300

    signin functionality is added.

--- restaurantApp/urls.py
+++ restaurantApp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -11,4 +12,6 @@ urlpatterns = [
     path('menu-item/<int:pk>/', views.display_menu_item, name="menu_item"),  
     path('bookings/', views.bookings, name='bookings'), 
     path('register/', views.register, name='register'), 
+    path('login/',auth_views.LoginView.as_view(template_name='signin.html'),name='signin'),
+    path('logout',auth_views.LogoutView.as_view(template_name='signout.html'),name='signout'),
 ]
